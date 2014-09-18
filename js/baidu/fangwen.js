b754707bcc9a3a11953b955cfa138a7afa7814de
huzhengfei <511388002@qq.com>
2014-09-18 23:55:48 +0800

    1.0

---
+++ js/baidu/fangwen.js
@@ -0,0 +1,3 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Ffebed01b29bcfe0ce795284839c5aba8' type='text/javascript'%3E%3C/script%3E"));
+
