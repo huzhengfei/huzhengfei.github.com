fb0e39fd9b4240efd0fd780abd22b16da3fdb319
huzhengfei <511388002@qq.com>
2014-09-21 17:05:31 +0800

    1

--- style/js/post.js
+++ style/js/post.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
     });
 
     (function(){
-        var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
+       
 
         function initHeading(){
             var h2 = [];
@@ -130,7 +130,7 @@ $(document).ready(function(){
             };
         })();
 
-        if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
+        if($('.entry h2').length > 2 && !isMobile.any() && ){
 
             genIndex();
 
